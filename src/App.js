55bf6b681cf5c9e0e7f497441f5b29ac22c1dffa
SimpleGRC <64481300+SimpleGRC@users.noreply.github.com>
2020-11-05 15:59:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          Under construction!
+          Under Construction, coming soon!
+          </p>
+          <p>
           Edit <code>src/App.js</code> and save to reload.
           </p>
+      
           <a
             className="App-link"
             href="https://reactjs.org"
