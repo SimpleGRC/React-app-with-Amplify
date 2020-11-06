e83f8c0796445927f7d00bfe9d24b436df4fab1b
SimpleGRC <64481300+SimpleGRC@users.noreply.github.com>
2020-11-05 22:39:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,18 +15,8 @@ class App extends Component {
           <p>
           Under Construction, coming soon!
           </p>
-          <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
       
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      
         </header>
       </div>
     );
