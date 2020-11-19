36842e84ac7452ddf6867ff2bda8d93f65d9ce2e
Pawel <p_wilczynski@salemstae.edu>
2020-11-19 16:38:20 -0500

    reverting uploader code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import S3FileUpload from 'react-s3';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
+import Amplify, { Auth } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
-//Optional Import
-import { uploadFile } from 'react-s3';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Under Construction, coming soon!
+        </p>
 
-const config = {
-    bucketName: 'simplegrc',
-    dirName: 'Evidence', /* optional */
-    region: 'eu-west-1',
-    accessKeyId: 'AKIAQ5FVT2TIN535D6US',
-    secretAccessKey: '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',
+
+        </header>
+      </div>
+    );
+  }
 }
 
-/*  Notice that if you don't provide a dirName, the file will be automatically uploaded to the root of your bucket */
-
-
-
-S3FileUpload
-    .uploadFile(file, config)
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
-
-//** OR *//
-
-uploadFile(file, config)
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
-
-  /**
-   * {
-   *   Response: {
-   *     bucket: "your-bucket-name",
-   *     key: "photos/image.jpg",
-   *     location: "https://your-bucket.s3.amazonaws.com/photos/image.jpg"
-   *   }
-   * }
-   */
-});
+export default withAuthenticator(App, true);
